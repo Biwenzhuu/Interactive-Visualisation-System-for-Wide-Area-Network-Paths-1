91ebb45ca4df05b5cac39791575043dc2cb2920a
biwen <biwen@kth.se>
2017-12-13 10:42:06 +0100

    1st commit

--- ippoxy.py
+++ ippoxy.py
@@ -9,7 +9,7 @@ from lxml import etree  #解析
 
 def GetUserAgent():
     '''
-    功能：随机获取HTTP_User_Agent
+    功能:随机获取HTTP_User_Agent
     '''
     user_agents=[
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
